b11f2d118d92f9492f9e930b7f5061c38e7e129f
Sajithmym <sajithmym@gmail.com>
2024-08-05 15:52:56 +0530

    try to run

---
+++ MySQL_CRUD/src/main/java/com/sajithmym/mysqlcrud/MySQLCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajithmym.mysqlcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySQLCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
